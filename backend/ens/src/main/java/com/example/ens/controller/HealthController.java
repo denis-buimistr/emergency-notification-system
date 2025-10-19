1be001dab9397b060c94a0c5da036ca527f83c87
Buimistr Denis <denis.buimistr@gmail.com>
2025-10-20 00:27:41 +0300

    feat: подключил PostgreSQL и запустил Spring Boot сервер

---
+++ backend/ens/src/main/java/com/example/ens/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.example.ens.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/")
+    public String health() {
+        return "ENS API is running 🚀";
+    }
+}
